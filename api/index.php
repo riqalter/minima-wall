bb10f435060682f32f9cda6140e624e23d5940ec
Mikail Thoriq <mikail.thoriq@gmail.com>
2023-02-21 19:06:54 +0700

    test experimental fallback implementation

--- api/index.php
+++ api/index.php
@@ -70,6 +70,8 @@ $BASE_URL = "https://raw.githubusercontent.com/$REPO/$BRANCH_NAME/$IMAGES_DIRECT
 // prefix buat generate thumnail 332px x 200px
 $IMGPROXY_PREFIX = "https://imgproxy1.riqexpe.eu.org/?url=$BASE_URL";
 
+$secondaryServerUrl = "https://imgproxy3.riqexpe.eu.org/?url=$BASE_URL";
+
 // api github buat return file dari repository
 $GITHUB_API_URL = "https://api.github.com/repos/$REPO/contents/$IMAGES_DIRECTORY/";
 
@@ -201,7 +203,7 @@ if (isset($_GET['random'])) {
 
     <div class="icons">
         <!-- GitHub -->
-        <a href="https://github.com/riqalter/minimal-wall" target="_blank" title="GitHub">
+        <a href="https://github.com/riqalter/minima-wall" target="_blank" title="GitHub">
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 496 512" height="2em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
             </svg>
@@ -278,7 +280,21 @@ if (isset($_GET['random'])) {
             });
         });
 
-        // jika server wsrv.nl gagal/error, fallback ke full-size image (klo udah ke fallback konsekuensi jadi lemot website nya)
+        // jika server IMGPROXY_PREFIX gagal/error, fallback ke server backup
+
+        document.querySelectorAll(".gallery img").forEach(function(img) {
+            img.addEventListener("error", function() {
+                if (this.src.indexOf(IMGPROXY_PREFIX) === 0) {
+                    // If the original URL has the IMGPROXY_PREFIX, fallback to the secondary server
+                this.src = secondaryServerUrl + this.src.substring(IMGPROXY_PREFIX.length);
+                } else {
+                    // Otherwise, fallback to the original link
+                this.src = this.parentElement.href;
+                }
+            });
+        });
+
+
         document.querySelectorAll(".gallery img").forEach(function(img) {
             img.addEventListener("error", function() {
                 this.src = this.parentElement.href;
